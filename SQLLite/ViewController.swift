1331986ce5f2baef4734f4c4b3493b5a27ba0bd3
Adam Roberts <Adam_roberts@gmx.com>
2022-02-16 10:53:12 -0500

    Initial Commit

---
+++ SQLLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQLLite
+//
+//  Created by Adam Roberts on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SQLLiteUITests/SQLLiteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SQLLiteUITestsLaunchTests.swift
+//  SQLLiteUITests
+//
+//  Created by Adam Roberts on 2/16/22.
+//
+
+import XCTest
+
+class SQLLiteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
